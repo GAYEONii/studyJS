116dc6336b29c72ce399e370f23ddf24729cc773
Gayeon Lee <rkdus1006@gmail.com>
2020-03-19 13:10:32 +0900

    js modify-2

--- basketball/scripts.js
+++ basketball/scripts.js
@@ -1,6 +1,7 @@
 var comScore = 0;
 var userScore = 0;
 var isComputerTurn = true; //누구의 차례인지 정하는 변수
+var shotsLeft = 15; //남은 횟수
 
 function onComputerShoot(){
     if(!isComputerTurn) //컴퓨터 차례가 아니라면 return
@@ -32,6 +33,16 @@ function onComputerShoot(){
     }
     
     isComputerTurn = false;
+
+    var computerButtons = document.getElementsByClassName('btn-computer');
+    for(var i=0; i<computerButtons.length; i++){
+        computerButtons[i].disabled = true;
+    }
+
+    var userButtons = document.getElementsByClassName('btn-user');
+    for(var i=0; i<userButtons.length; i++){
+        userButtons[i].disabled = false;
+    }
 }
 
 function onUserShoot(shootType){
@@ -64,4 +75,39 @@ function onUserShoot(shootType){
     }
 
     isComputerTurn = true;
+
+    var computerButtons = document.getElementsByClassName('btn-computer');
+    for(var i=0; i<computerButtons.length; i++){
+        computerButtons[i].disabled = false;
+    }
+
+    var userButtons = document.getElementsByClassName('btn-user');
+    for(var i=0; i<userButtons.length; i++){
+        userButtons[i].disabled = true;
+    }
+
+    shotsLeft--;
+
+    var shotsLeftElem = document.getElementById('shots-left');
+    shotsLeftElem.innerHTML = shotsLeft;
+
+    if(shotsLeft===0){
+        if(userScore > comScore){
+            textElem.innerHTML = '승리했습니다^^';
+        }
+        else if(userScore < comScore){
+            textElem.innerHTML = '졌습니다 ㅠㅠ';
+        }
+        else{
+            textElem.innerHTML = '비겼습니다.';
+        }
+
+        for(var i=0; i<computerButtons.length; i++){
+            computerButtons[i].disabled = true;
+        }
+        for(var i=0; i<userButtons.length; i++){
+            userButtons[i].disabled = true;
+        }
+    }
+
 }
