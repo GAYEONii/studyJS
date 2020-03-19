73088721f97a2a12934f5d89dc9daeaee1a76163
Gayeon Lee <rkdus1006@gmail.com>
2020-03-20 00:01:00 +0900

    vanillaJS example study

---
+++ vanillaJS_study/index.js
@@ -0,0 +1,19 @@
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked"
+function hasClass(){
+	/*if(title.classList.contains(CLICKED_CLASS)){
+		title.classList.remove(CLICKED_CLASS);
+	}
+	else{
+		title.classList.add(CLICKED_CLASS);
+	}*/
+	title.classList.toggle(CLICKED_CLASS);
+}
+
+function init(){
+	title.addEventListener("click", hasClass);
+}
+
+
+init();
