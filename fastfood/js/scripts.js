7fb71274aba4486c6c3d51197807fa46b7f9991c
Gayeon Lee <rkdus1006@gmail.com>
2020-03-20 13:06:18 +0900

    +search function, click function

--- fastfood/js/scripts.js
+++ fastfood/js/scripts.js
@@ -1,34 +1,44 @@
 const API_URL = 'https://floating-harbor-78336.herokuapp.com/fastfood';
 
+//검색 버튼을 클릭하는 함수
 $(function(){
     $('.btn-search').click(function(){
-        $.get(API_URL, {}, function(data){
-            var list = data.list;
-            var total = data.total;
-        
-            $('.total').html('총 '+total+'개의 패스트푸드점을 찾았습니다.');
+        var searchKeyword = $('#txt-search').val();
 
-            var $list = $('.list');
-
-            for(var i=0; i<list.length; i++){
-                var item = list[i];
-                //각 아이템 하나하나마다 DOM객체를 만들어서 $list에 추가한다.
-
-                //1. 템플릿을 복제한다.
-                var $elem = $('#item-template')
-                    .clone()
-                    .removeAttr('id');
-                //2. 복제한 템플릿에 데이터를 세팅한다.
-                    $elem.find('.item-no').html(i+1);
-                    $elem.find('.item-name').html(item.name);
-                    $elem.find('.item-addr').html(item.addr);
-
-                    $list.append($elem);
-                //3. 목록에 복제한 템플릿을 추가한다.
-            }
-        });
+        search(searchKeyword);
     });
-});
 
+    $('#txt-search').on('keypress',function(e){
+        if(e.keyCode === 13){
+            $('.btn-search').trigger('click');
+        }
+    });
+});    
 
+//실제로 서버에 요청을 보내주는 함수
+function search(searchKeyword){
+    $.get(API_URL, {
+        searchKeyword: searchKeyword
+    }, function(data){
+        var list = data.list;
+        var total = data.total;
 
+        $('.total').html('총 '+total+'개의 패스트푸드점을 찾았습니다.');
+
+        var $list = $('.list');
+
+        for(var i=0; i<list.length; i++){
+            var item = list[i];
+
+            var $elem = $('#item-template')
+                .clone()
+                .removeAttr('id');
+
+            $elem.find('.item-no').html(i+1);
+            $elem.find('.item-name').html(item.name);
+            $elem.find('.item-addr').html(item.addr);
+
+            $list.append($elem);
+        }
+    });
+}
