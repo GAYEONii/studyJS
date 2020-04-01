5f64813a2ede0e03812f3c937d56bb9ad6f5d6b2
Gayeon Lee <rkdus1006@gmail.com>
2020-04-01 15:25:20 +0900

    다시시작 버튼 추가+ count 수정

--- inflearn/숫자야구/baseball.js
+++ inflearn/숫자야구/baseball.js
@@ -6,6 +6,9 @@ var action = document.querySelector('.action');
 var input = document.querySelector('input');
 var text = document.querySelector('.result');
 var chance = document.querySelector('#chance');
+var restart = document.querySelector('#restart');
+
+var count = 10;
 
 function drawing(){
     var number = [1,2,3,4,5,6,7,8,9];
@@ -25,11 +28,18 @@ function drawing(){
 //배열.join(구분자) -> 문자
 
 //배열.indexof(값) -> 값의 위치를 알 수 있음 (없다면 -1)
-var answer = drawing();
-var count = 10;
 
-action.addEventListener('submit',function game(e){
+
+var answer = drawing();
+
+//숫자 입력 후 입력 버튼 클릭 시
+action.addEventListener('submit',function (e){
     e.preventDefault();
+    score(answer);
+});
+
+//strike, ball 판단
+function score(answer){
     var word = input.value;
     if(word===answer.join('')){
         text.textContent='홈런!!';
@@ -48,14 +58,27 @@ action.addEventListener('submit',function game(e){
             }
         }
         text.textContent=strike+'스트라이크 '+ball+'볼';
-        if(count>0){
-            count--;
-            
-            chance.textContent='남은 기회 : '+count+'번';
-        } else{
-            text.textContent='실패ㅠㅠ';
-        }
-        
+        countFunc(count);
+        count--;
     }
+    input.value='';
+}
 
-});
+//다시 시작 버튼 클릭 시
+function button_click(){
+    count = 10;
+    input.value='';
+    answer = drawing();
+    score(answer);
+}
+
+//남은 기회 count
+function countFunc(count){
+    if(count>0){
+        chance.textContent='남은 기회: '+count+'번';
+        
+    } else{
+        chance.textContent='남은 기회: 0번';
+        text.textContent='실패ㅠㅠ';
+    }
+}
