05ac3f0baa51e7a93efa3480c678c721e3d0d096
Gayeon Lee <rkdus1006@gmail.com>
2020-04-03 15:46:00 +0900

    color함수 정리 + 보너스 볼 추가

--- inflearn/로또추첨기/lotto.js
+++ inflearn/로또추첨기/lotto.js
@@ -15,40 +15,32 @@ while(lotto.length>0){
    var num = lotto.splice(Math.floor(Math.random()*lotto.length),1)[0];
    shuffle.push(num);
 }
-console.log(shuffle);
 
+//로또 6개 가져와서 숫자에 색 입히기
 for(var i=0; i<6; i++){
     lottoNum[i].textContent = shuffle[i];
-    var colorNum = Number(lottoNum[i].textContent);
-
-    if(color(colorNum)===1){
-        lottoNum[i].style.backgroundColor='red';
-    }else if(color(colorNum)===2){
-        lottoNum[i].style.backgroundColor='orange';
-    }else if(color(colorNum)===3){
-        lottoNum[i].style.backgroundColor='yellow';
-    }
-    else if(color(colorNum)===4){
-        lottoNum[i].style.backgroundColor='blue';
-    }
-    else if(color(colorNum)===5){
-        lottoNum[i].style.backgroundColor='green';
-    }
+    var colorNum = lottoNum[i].textContent;
+    color(colorNum, lottoNum[i]);
 }
 
-//숫자 색
-function color(element){
+//숫자 색 정하는 함수
+function color(element, lottoNum){
     if(element<=10){
-        return 1;
+        lottoNum.style.backgroundColor='red';
     }else if(element>=11 && element<=20){
-        return 2;
+        lottoNum.style.backgroundColor='orange';
     }
     else if(element>=21 && element<=30){
-        return 3;
+        lottoNum.style.backgroundColor='yellow';
     }
     else if(element>=31 && element<=40){
-        return 4;
+        lottoNum.style.backgroundColor='blue';
     }else{
-        return 5;
+        lottoNum.style.backgroundColor='green';
     }
 }
+
+//보너스 볼 추첨
+var bonusBall = shuffle[shuffle.length-1];
+lottoNum[lottoNum.length-1].textContent = bonusBall;
+color(bonusBall, lottoNum[lottoNum.length-1]);
